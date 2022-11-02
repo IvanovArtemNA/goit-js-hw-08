484da6a4b1f4d786b326e4565bb53267e94be9d7
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-02 18:57:05 +0200

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img loading="lazy" class="gallery__image" 
+       src="${preview}" 
+       alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+const galleryLighBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  //   overlayOpacity: 1,
+  //   maxZoom: 2,
+});
+
+galleryLighBox.show();
