dea8f77e998354da9b0535857b1c52b13a03ea27
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-09 22:21:48 +0200

    HW_8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const playerTime = localStorage.getItem('videoplayer-current-time');
+
+const onPlayerPlayTime = throttle(({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+// setItem(key, value) - створює новий, або оновлює вже існуючий запис у сховищі.
+
+player.on('timeupdate', onPlayerPlayTime);
+
+player.setCurrentTime(playerTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', throttle(onFeedbackForm, 500));
+formEl.addEventListener('submit', onSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+newFormData();
+
+function onSubmit(event) {
+  event.preventDefault();
+  if (formEl.elements.email.value && formEl.elements.message.value) {
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    // оставь вывод
+    console.log(formData);
+  }
+}
+
+function onFeedbackForm() {
+  formData.email = formEl.elements.email.value;
+  formData.message = formEl.elements.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function newFormData() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY) || '');
+    formEl.elements.email.value = formData.email;
+    formEl.elements.message.value = formData.message;
+  }
+}
